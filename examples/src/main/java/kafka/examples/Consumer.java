7975488e378c87f70aedf7905ee05a55baf0f8a0
shaolei <1767652530@qq.com>
2021-11-04 20:54:37 +0800

    commit comment

--- examples/src/main/java/kafka/examples/Consumer.java
+++ examples/src/main/java/kafka/examples/Consumer.java
@@ -47,6 +47,7 @@ public class Consumer extends Thread{
         consumer = new KafkaConsumer<>(props);
         this.topic = topic;
     }
+
     public void run(){
         // 消费者再平衡监听器
         //consumer.subscribe(Pattern pattern, ConsumerRebalanceListener listener)
